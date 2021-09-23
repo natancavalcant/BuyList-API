9b6d2ed3cee18c2b05658580fa023fb9cbf317d3
natan <natancavalcant@gmail.com>
2021-09-23 13:18:20 -0300

    feat: some-alterations

--- config/database.js
+++ config/database.js
@@ -1,10 +1,23 @@
 require("dotenv/config")
 
 module.exports = {
-  username: process.env.USERNAME_DB,
-  password: process.env.PASSWORD_DB,
-  database: process.env.DATABASE_DB,
-  host: process.env.DATABASE_URL,
-  port: process.env.PORT_DB,
-  dialect: process.env.DIALECT_DB,
+  "test": {
+    username: process.env.USERNAME_DB,
+    password: process.env.PASSWORD_DB,
+    database: process.env.DATABASE_DB,
+    host: process.env.DATABASE_URL,
+    port: process.env.PORT_DB,
+    dialect: process.env.DIALECT_DB,
+  },
+  "production": {
+    "use_env_variable": "DATABASE_URL"
+  },
+  "development": {
+    username: process.env.USERNAME_DB,
+    password: process.env.PASSWORD_DB,
+    database: process.env.DATABASE_DB,
+    host: process.env.DATABASE_URL,
+    port: process.env.PORT_DB,
+    dialect: process.env.DIALECT_DB,
+  }
 }
