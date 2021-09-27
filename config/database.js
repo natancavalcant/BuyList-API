77dcfda4053b4b022ad3e67cefcfc9e26d68ab9b
natan <natancavalcant@gmail.com>
2021-09-27 15:13:28 -0300

    ready to deploy

--- config/database.js
+++ config/database.js
@@ -4,8 +4,7 @@ module.exports = {
   username: process.env.USERNAME_DB,
   password: process.env.PASSWORD_DB,
   database: process.env.DATABASE_DB,
-  host: process.env.DATABASE_URL,
+  host: process.env.HOST_DB,
   dialect: process.env.DIALECT_DB,
   port: process.env.PORT_DB,
-  use_env_variable: "DATABASE_URL"
 }
