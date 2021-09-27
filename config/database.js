e7502aeb9075a9768bb6f335b08763065b6c45b5
natan <natancavalcant@gmail.com>
2021-09-27 16:22:09 -0300

    ready to deploy

--- config/database.js
+++ config/database.js
@@ -1,6 +1,13 @@
 require("dotenv/config")
 
-module.exports = {
+module.exports = process.env.NODE_ENV == 'test' ? {
+  username: process.env.USERNAME_DB,
+  password: process.env.PASSWORD_DB,
+  database: process.env.DATABASE_DB,
+  host: process.env.HOST_DB,
+  dialect: process.env.DIALECT_DB,
+
+} : {
   username: process.env.USERNAME_DB,
   password: process.env.PASSWORD_DB,
   database: process.env.DATABASE_DB,
