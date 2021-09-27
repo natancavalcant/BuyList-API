32a8d0e64b58bd347e00a17abd0f36da1a3d7b8a
natan <natancavalcant@gmail.com>
2021-09-27 09:11:40 -0300

    feat:some-alterations

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 require("dotenv/config")
 
 module.exports = {
-  dialect: process.env.DIALECT_DB,
+  dialect: "postgres",
   username: process.env.USERNAME_DB,
   password: process.env.PASSWORD_DB,
   database: process.env.DATABASE_DB,
   host: process.env.DATABASE_URL,
-  port: "postgres",
+  port: process.env.PORT_DB,
 }
